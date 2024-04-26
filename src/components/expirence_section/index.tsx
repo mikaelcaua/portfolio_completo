84072411d5d7fabb2fcf9535cec7739bfa3c3484
Mikael Cauã <mikaelcauasilva1@gmail.com>
2024-04-26 08:37:11 -0300

    backup

--- src/components/expirence_section/index.tsx
+++ src/components/expirence_section/index.tsx
@@ -14,7 +14,7 @@ export default function ExpirenceSection({title,children,src,reverse}:ExpirenceS
         <section className="p-[2rem] flex items-center flex-col w-[100%] gap-[0.5rem] ">
             <h1 className="text-text_color text-[2.3rem] font-lexend font-bold">{title}</h1>
             <div className={`flex ${reverseImage[0]} justify-center gap-[1rem]`}>
-                <Image className="rounded-[0.5rem]" alt={title} src={src} width={250} height={250} style={{ boxShadow: "#8821e7 "+reverseImage[1]+" 0.7rem 1px"}}></Image>
+                <Image className="rounded-[0.5rem]" alt={title} src={src} width={250} height={250} style={{ boxShadow: "var(--primary_color)"+reverseImage[1]+" 0.7rem 1px"}}></Image>
                 
                 <p className="text-white h-[70%] break-words max-w-[60vw] text-[1.5rem] font-laila pt-[1rem]">
                     {children}
