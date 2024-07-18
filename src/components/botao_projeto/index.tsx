f1fa3377bd08bbbd76ed3958491b5d21d68a985e
mikaelcaua <mikaelcauasilva1@gmail.com>
2024-07-18 18:47:32 -0300

    atualizando botao visitar

--- src/components/botao_projeto/index.tsx
+++ src/components/botao_projeto/index.tsx
@@ -5,7 +5,7 @@ import Button from '@mui/material/Button';
 export default function BotaoProjeto({href}:{href:string}) {
   return (
     <Stack spacing={2} direction="row">
-      <a target="_blank" href={href}><Button variant="contained" className="bg-button_color font-semibold ">Visitar Projeto</Button></a>
+      <a target="_blank" href={href}><Button variant="contained" className="bg-button_color font-semibold ">VISIT PROJECT</Button></a>
     </Stack>
   );
 }
